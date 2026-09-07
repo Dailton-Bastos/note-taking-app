2d584ac229962f7e1caf1798a55f78494c0a374d
Dailton Bastos <dailtonbastos@gmail.com>
2026-09-07 17:55:40 -0300

    chore: remove comments end-to-end tests config

--- apps/api/vitest.config.e2e.ts
+++ apps/api/vitest.config.e2e.ts
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vitest/config';
-// import tsconfigPaths from 'vite-tsconfig-paths';
-
-// export default defineConfig({
-//   plugins: [tsconfigPaths()],
-//   test: {
-//     globals: true,
-//     root: './',
-//     include: ['**/*.e2e-spec.ts'],
-//   },
-// });
 import { apiE2EConfig } from '@repo/vitest-config';
 
 export default apiE2EConfig;
