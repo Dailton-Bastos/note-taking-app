d8d995f9f9f4fab4f0ec12b44b587d1afe5fb2da
Dailton Bastos <dailtonbastos@gmail.com>
2026-09-20 22:19:31 -0300

    feat(env): add validation schema for environment variables

--- packages/schemas/src/env/index.ts
+++ packages/schemas/src/env/index.ts
@@ -1,5 +1,14 @@
+import { z } from 'zod';
+
 export enum Environment {
   Development = 'development',
   Production = 'production',
   Test = 'test',
 }
+
+export const validationApiSchema = z.object({
+  NODE_ENV: z
+    .enum([Environment.Development, Environment.Production, Environment.Test])
+    .default(Environment.Development),
+  PORT: z.coerce.number<number>().default(3000),
+});
