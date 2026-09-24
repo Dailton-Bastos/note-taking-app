07dfdd593770cfcee72078cbfb949a65beb6a9de
Dailton Bastos <dailtonbastos@gmail.com>
2026-09-23 22:26:02 -0300

    feat(api): retrieve PORT from EnvService for application startup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { EnvService } from './env/env.service.js';
+import { EnvModule } from './env/env.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const envService = app.select(EnvModule).get(EnvService, { strict: true });
+
+  const PORT = envService.get('PORT');
+
+  await app.listen(PORT);
 }
 await bootstrap();
